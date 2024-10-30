d8ce0ef577e7ed0f943e65c9fd4c80c39cf44f40
Fl0rianCh <44348407+Fl0rianCh@users.noreply.github.com>
2024-10-30 22:48:54 +0100

    Update bot_arbitrage.py

--- bot_arbitrage.py
+++ bot_arbitrage.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import logging
 from binance.client import Client
-from binance.websockets import BinanceSocketManager
 from binance.streams import BinanceSocketManager
 from telegram import Bot
 from datetime import datetime, timedelta
